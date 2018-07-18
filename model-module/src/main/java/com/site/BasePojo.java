c044cb17594e0a20ca60fca89d64eee4f874da37
Raul Sanchez <raulsanchez1981@gmail.com>
2018-07-18 14:49:35 +0200

    New site

---
+++ model-module/src/main/java/com/site/BasePojo.java
@@ -0,0 +1,14 @@
+package com.site;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Created by rsanchpa on 18/07/2018.
+ */
+public class BasePojo {
+
+    private String id;
+    private LocalDate AddedDate;
+
+}
